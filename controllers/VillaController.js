f6e1ae21299ea07d2eddf608aea22ce4b0d4cad9
rizkiazkafihiaghnia996 <rizkiazkafihiaghnia996@gmail.com>
2023-05-09 21:24:08 +0700

    stored progress

--- controllers/VillaController.js
+++ controllers/VillaController.js
@@ -56,7 +56,47 @@ class VillaController {
     }
 
     static async update(request, response) {
-        
+        try {
+            const id = +request.params.id;
+            const roleAuth = request.userData.role;
+
+            if (roleAuth !== 'Admin') {
+                response.status(403).json({
+                    status: false,
+                    message: 'Only admins are allowed to do this action'
+                });
+            } else {
+                const { LocationId, name, description, price, map_url, phone, bedroom, bathroom, swimming_pool } = request.body;
+                let defaultData = await Villa.findByPk(id);
+
+                let result = await Villa.update({
+                    LocationId: LocationId === undefined ? defaultData.LocationId : LocationId,
+                    name: name === undefined ? defaultData.name : name,
+                    description: description === undefined ? defaultData.description : description,
+                    price: price === undefined ? defaultData.price : price,
+                    map_url: map_url === undefined ? defaultData.map_url : map_url,
+                    phone: phone === undefined ? defaultData.phone : phone,
+                    bedroom: bedroom === undefined ? defaultData.bedroom : bedroom,
+                    bathroom: bathroom === bathroom ? defaultData.bathroom : bathroom,
+                    swimming_pool: swimming_pool === undefined ? defaultData.swimming_pool : swimming_pool
+                }, {
+                    where: {id}
+                });
+
+                result[0] === 1 ? response.status(200).json({
+                    status: true,
+                    message: `Villa with an ID of ${id} has been updated`
+                }) : response.status(404).json({
+                    status: true,
+                    message: `Villa with an ID of ${id} couldn't be updated or wasn't found`
+                });
+            }
+        } catch(err) {
+            response.status(500).json({
+                status: false,
+                message: String(err)
+            })
+        }
     }
 }
 
--- routes/villasRoutes.js
+++ routes/villasRoutes.js
@@ -4,5 +4,6 @@ const { auth } = require('../middlewares/auth');
 
 villasRoutes.get('/', auth, VillaController.getAll);
 villasRoutes.post('/add', auth, VillaController.add);
+villasRoutes.put('/update/:id', auth, VillaController.update);
 
 module.exports = villasRoutes;

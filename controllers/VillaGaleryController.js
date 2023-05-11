909a938735187d6e5ef5b51d25422e8b5e843cc2
rizkiazkafihiaghnia996 <rizkiazkafihiaghnia996@gmail.com>
2023-05-11 13:24:28 +0700

    ...

--- controllers/VillaGaleryController.js
+++ controllers/VillaGaleryController.js
@@ -25,7 +25,22 @@ class VillaGaleryController {
 
     static async getByVilla(request, response) {
         try {
+            const VillaId = +request.params.VillaId;
 
+            let result = await VillaGalery.findAll({
+                order: [
+                    ['id', 'asc']
+                ],
+                where: {
+                    VillaId: VillaId
+                }
+            });
+
+            response.status(200).json({
+                status: true,
+                message: `VillaGallery for Villa with an ID of  ${VillaId} fetched!`,
+                data: result
+            });
         } catch(err) {
             response.status(500).json({
                 status: false,
@@ -62,6 +77,39 @@ class VillaGaleryController {
                     status: true,
                     message: `Images for Villa with an ID of ${VillaId} have been added`,
                     data: resultArr
+                });
+            }
+        } catch(err) {
+            response.status(500).json({
+                status: false,
+                message: String(err),
+                data: null
+            });
+        }
+    }
+
+    static async delete(request, response) {
+        try {
+            const roleAuth = request.userData.role;
+            const id = +request.params.id;
+
+            if(roleAuth !== 'Admin') {
+                response.status(403).json({
+                    status: false,
+                    message: 'Only admins are allowed to do this action',
+                    data: null
+                });
+            } else {
+                let result = await VillaGalery.destroy({
+                    where: {id}
+                });
+    
+                result === 1 ? response.status(200).json({
+                    status: true,
+                    message: `VillaGallery with an ID of ${id} has been deleted`
+                }) : response.status(404).json({
+                    status: false,
+                    message: `VillaGallery with an ID of ${id} couldn't be deleted or wasn't found`
                 });
             }
         } catch(err) {
